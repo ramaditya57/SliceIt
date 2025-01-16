fa7f7a513ad3722b16349ea32a27b681a99aff2d
ramaditya57 <ramadityachaudhary@gmail.com>
2025-01-16 22:32:09 +0530

    Updated Frontend

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SliceIt/', // Add this line for GitHub Pages deployment
+  base: '/SliceIt/', // Match the repository name exactly
 });
