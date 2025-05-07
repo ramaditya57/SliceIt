61216f409135776385534fc75b9ddba636072b0e
ramaditya57 <ramadityachaudhary@gmail.com>
2025-05-07 14:06:06 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
